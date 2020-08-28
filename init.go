e4366d8a8c36fe5adcda32fc8e53f34c5f00b3c6
Thomas Björk <micoxdev@gmail.com>
2020-08-28 18:33:48 +0200

    Minor changes to the auth Started to add some static files for handling

--- init.go
+++ init.go
@@ -3,11 +3,23 @@ package main
 import (
 	EComApp "github.com/codedv8/go-ecom-app"
 	EComStructs "github.com/codedv8/go-ecom-structs"
+	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
+	"log"
 )
 
 func (admin *ADMIN) SysInit(app *EComApp.Application) {
-
+	// Create apiRouter
+	adminRouter := app.Router.Group("/admin")
+	// Create basic auth object
+	basicAuth := app.UseBasicAuth("Admin")
+	// Add basic auth to adminRouter
+	adminRouter.Use(basicAuth.Use())
+	// Add handlers
+	adminRouter.Handle("GET", "/", func(c *gin.Context) {
+		log.Printf("Welcom to this admin %+v\n", c)
+		c.String(200, "Welcome to this admin")
+	})
 }
 
 func (admin *ADMIN) Init(app *EComApp.Application) {
@@ -15,8 +27,9 @@ func (admin *ADMIN) Init(app *EComApp.Application) {
 		switch c := payload.(type) {
 		case *EComStructs.RouterWildcard:
 			path := c.Context.Request.URL.Path
-			if len(path) == 11 {
-				c.Context.String(200, "Path was 11 chars in length. Plugin Admin responded to this call")
+			if path[:7] == "/admin/" {
+				c.Context.String(200, c.Context.Request.Method+" "+path[7:]+"\n")
+				c.Context.String(200, "Plugin Admin responded to this call: "+path)
 				return false, nil
 			}
 			return true, nil
